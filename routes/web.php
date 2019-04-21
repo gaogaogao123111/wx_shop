48e8178f0408275cfe58ecd666458551084c7d5d
gaogaogao123111 <17610531571@163.com>
2019-04-21 16:43:22 +0000

    日志

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/Order/orderlist','OrderController@orderList');//订单列表
 Route::get('/Order/paystatus','OrderController@paystatus');      //订单支付状态
 
 Route::get('/Pay/wxpay', 'Weixin\WxPayController@wxpay');      //微信支付
-Route::post('/Weixin/notify', 'Weixin\WxPayController@notify');      //支付通知回调
+Route::get('/Weixin/notify', 'Weixin\WxPayController@notify');      //支付通知回调
 
 
 Route::get('/Weixin/paysuccess', 'Weixin\WxPayController@paysuccess');      //支付成功
