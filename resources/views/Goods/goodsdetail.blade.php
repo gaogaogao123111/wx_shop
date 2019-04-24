7ef3ea2586eddda076a56dfefd79664197c08566
gxd <17610531571@163.com>
2019-04-24 05:39:21 +0000

    分享

--- resources/views/Goods/goodsdetail.blade.php
+++ resources/views/Goods/goodsdetail.blade.php
@@ -83,6 +83,29 @@
             <h3 style="color: red">ID:{{$v['goods_id']}}----------商品名称：{{$v->goods_name}}----------商品价格：￥{{$v->self_price}}</h3>
         @endforeach
     </div>
+    <script src="/js/jquery/jquery-1.12.4.min.js"></script>
+    <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
+    <script>
+        wx.config({
+            // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: "{{$jsconfigsdk['appId']}}", // 必填，公众号的唯一标识
+            timestamp: "{{$jsconfigsdk['timestamp']}}", // 必填，生成签名的时间戳
+            nonceStr: "{{$jsconfigsdk['nonceStr']}}", // 必填，生成签名的随机串
+            signature: "{{$jsconfigsdk['signature']}}",// 必填，签名
+            jsApiList: ['chooseImage','uploadImage'] // 必填，需要使用的JS接口列表
+        });
+        wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+            wx.updateAppMessageShareData({
+                title: '最新', // 分享标题
+                desc: '1', // 分享描述
+                link: '1809gaoxiangdong.comcto.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: '@', // 分享图标
+                success: function (res) {
+                    console.log(res);
+                }
+            })
+        });
+    </script>
 </div>
 </body>
 </html>
