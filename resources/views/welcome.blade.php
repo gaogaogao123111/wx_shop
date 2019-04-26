093680fee763a673dc2403c48f098ba2db05dea1
gxd <17610531571@163.com>
2019-04-26 05:48:47 +0000

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    awx_shopLaravel
+                    wx_shopLaravel
                 </div>
 
                 <div class="links">
